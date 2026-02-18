dac092db22d72a3d659bfe386d20a6f8ae3ae5ad
RohanRaj <rohanraj.madanu@gmail.com>
2026-02-18 23:26:25 +0530

    updated frontend

--- react-nginx/src/App.js
+++ react-nginx/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and fuck to reload.
+          Edit <code>src/App.js</code> and click to reload.
         </p>
         <a
           className="App-link"
