d331448122b25bf5807c8eb368e6828dcd69e421
RohanRaj <rohanraj.madanu@gmail.com>
2026-03-03 15:20:00 +0530

    testing build triggers

--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! This is earth 3'
+    return 'Hello, world! testing AWS CodePipeline'
 
 if __name__ == '__main__':
     app.run()
