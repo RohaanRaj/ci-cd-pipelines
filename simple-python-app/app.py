9023920143ec07520f3337687fbb66b3a519587e
RohaanRaj <rohanraj.madanu@gmail.com>
2026-03-03 14:48:24 +0530

    build trigger 2

--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! This is earth'
+    return 'Hello, world! This is earth 2'
 
 if __name__ == '__main__':
     app.run()
