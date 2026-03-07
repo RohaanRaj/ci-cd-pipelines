41ce8a21f79a9a0ac295579ae3a404653cb308a9
RohanRaj <rohanraj.madanu@gmail.com>
2026-03-07 21:20:51 +0530

    added deployment files

--- voting-app/vote/app.py
+++ voting-app/vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Renu")
-option_b = os.getenv('OPTION_B', "Rutika")
+option_a = os.getenv('OPTION_A', "Rutika")
+option_b = os.getenv('OPTION_B', "Pranita")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
